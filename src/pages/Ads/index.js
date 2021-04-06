277dcbb8173a47cd9f0cdeb3ef3dba3caf46932c
Christopher de Oliveira <christopherforwork@gmail.com>
2021-04-06 11:59:32 -0400

    🚧 (Pages): Started structuring the filter side

--- src/pages/Ads/index.js
+++ src/pages/Ads/index.js
@@ -2,15 +2,30 @@ import React, { useState, useEffect } from 'react'
 import useApi from '../../helpers/OLXApi';
 import { PageArea } from './style';
 import { PageContainer } from '../../components';
-import { Link } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import { AdItem } from '../../components';
 
 const Page = () => {
   const api = useApi();
 
+  const useQueryString = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  const query = useQueryString();
+
+  const [q, setQ] = useState('');
+  const [cat, setCat] = useState('');
+  const [state, setState] = useState('');
   const [stateList, setStateList] = useState([]);
   const [categories, setCategories] = useState([]);
   const [adList, setAdList] = useState([]);
+
+  useEffect(() => {
+    setQ(query.get('q'));
+    setCat(query.get('cat'));
+    setState(query.get('state'));
+  }, [query]);
 
   useEffect(() => {
     const getStates = async () => {
@@ -54,10 +69,15 @@ const Page = () => {
         <PageArea>
           <div className="leftSide">
             <form method="GET">
-              <input type="text" name="q" />
+              <input
+                type="text"
+                name="q"
+                placeholder="O que você procura?"
+                value={q}
+              />
 
               <div className="filterName">Estado:</div>
-              <select name="state">
+              <select name="state" value={state}>
                 <option></option>
                 {stateList.map((item, key) => (
                   <option key={key} value={item.uf}>{item.uf}</option>
@@ -67,7 +87,7 @@ const Page = () => {
               <div className="filterName">Categoria:</div>
               <ul>
                 {categories.map((item, key) => (
-                  <li key={key} className="categoryItem">
+                  <li key={key} className={`categoryItem ${cat === item.slug && 'active'}`}>
                     <img src={item.img} alt={item.name} />
                     <span>{item.name}</span>
                   </li>
--- src/pages/Ads/style.js
+++ src/pages/Ads/style.js
@@ -7,6 +7,57 @@ export const PageArea = styled.div`
   .leftSide {
     width: 250px;
     margin-right: 10px;
+
+    .filterName {
+      font-size: 15px;
+      margin: 10px 0;
+    };
+
+    input, select {
+      width: 100%;
+      height: 40px;
+      border: 2px solid #9bb83c;
+      border-radius: 5px;
+      outline: 0;
+      font-size: 15px;
+      color: #000;
+      padding: 10px;
+    };
+
+    select {
+      cursor: pointer;
+      background-color: #fff;
+    };
+
+    ul, li {
+      margin: 0;
+      padding: 0;
+      list-style: none;
+    };
+
+    .categoryItem {
+      display: flex;
+      align-items: center;
+      padding: 10px;
+      border-radius: 5px;
+      color: 5px;
+      cursor: pointer;
+
+      img {
+        width: 25px;
+        height: 25px;
+        margin-right: 5px;
+      };
+
+      span {
+        font-size: 14px;
+      };
+    };
+
+    .categoryItem:hover, .categoryItem.active {
+      background-color: #9bb83c;
+      color: #fff;
+    };
   };
 
   .rightSide {
